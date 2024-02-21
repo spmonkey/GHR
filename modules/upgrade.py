fdc126d4f5ad88822a08938e9c8b64808b15d909
spmonkey <1015468607@qq.com>
2024-02-21 15:40:15 +0800

    Update upgrade.py

--- modules/upgrade.py
+++ modules/upgrade.py
@@ -18,19 +18,30 @@ GitHub:
 import wget
 import zipfile
 import os
+import shutil
 
 
 class up:
+
     def ghr_upgrade(self):
         try:
             url = "https://mirror.ghproxy.com/https://github.com/spmonkey/GHR/archive/refs/heads/main.zip"
-            filename = wget.download(url, "../GHR.zip")
+            filename = wget.download(url, "../GHR.zip", bar=False)
             with zipfile.ZipFile(filename, 'r') as zip_ref:
                 zip_ref.extractall("../")
 
             os.remove(filename)
+            path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+            self.recursive_folder(path)
             return True
         except:
-            print("\n [-] 更新失败，但是不影响使用，如果需要更新，请重新运行：python GHR.py --upgrade\n")
+            print(" [-] 更新失败，但是不影响使用，如果需要更新，请重新运行：python GHR.py --upgrade\n")
             return False
 
+    def recursive_folder(self, path):
+        for item in os.listdir(path):
+            item_path = os.path.join(path, item)
+            if item == "__pycache__":
+                shutil.rmtree(item_path)
+            if os.path.isdir(item_path) and item != "__pycache__":
+                self.recursive_folder(item_path)
